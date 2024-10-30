c39c37b95d3b1851f892b8f11de1da592e4df6ef
Roman Chermyshov <romarioriw2005@gmail.com>
2024-10-30 14:19:48 +0200

    Add Vivario module

--- bs_vivario_addon/__manifest__.py
+++ bs_vivario_addon/__manifest__.py
@@ -1,17 +1,44 @@
 {
-    'name': 'BS Vivario Addon',
-    'version': '1.1',
+    'name': 'BonSens Vivario add-on',
+    'summary': """
+        BonSens add-on for Inventory""",
+    'description': """
+        BonSens add-on for Inventory""",
+    'author': "BonSens",
+    'website': "https://www.bonsens.com.ua",
     'category': 'Inventory',
-    'summary': 'Adds "Закрито документами" status to delivery orders',
-    'description': 'This module adds a final status "Закрито документами" and a flag to Delivery Orders in Odoo.',
-    'website': 'https://www.odoo.com/app/inventory',
+    'version': '17.0.1.0.1',
     'depends': ['stock'],
     'data': [
          'views/delivery_order_view.xml',
-         'security/ir.model.access.csv',
     ],
+    'assets': {
+        'web.report_assets_common': [
+            'stock/static/src/scss/report_stock_reception.scss',
+            'stock/static/src/scss/report_stock_rule.scss',
+        ],
+        'web.assets_backend': [
+            'stock/static/src/**/*.js',
+            'stock/static/src/**/*.xml',
+            'stock/static/src/scss/*.scss',
+            'stock/static/src/views/**/*',
+        ],
+        'web.assets_frontend': [
+            'stock/static/src/scss/stock_traceability_report.scss',
+        ],
+        'web.assets_tests': [
+            'stock/static/tests/tours/*.js',
+        ],
+        'web.qunit_suite_tests': [
+            'stock/static/tests/counted_quantity_widget_tests.js',
+            'stock/static/tests/inventory_report_list_tests.js',
+            'stock/static/tests/popover_widget_tests.js',
+            'stock/static/tests/stock_traceability_report_backend_tests.js',
+            'stock/static/tests/stock_move_one2many_tests.js',
+        ],
+    },
+    'license': 'LGPL-3',
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
     'auto_install': True,
 }
--- bs_vivario_addon/models/delivery_order.py
+++ bs_vivario_addon/models/delivery_order.py
@@ -1,12 +1,12 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    closed_by_documents = fields.Boolean(string="Закрито документами", default=False)
-    original_document_number = fields.Char(string="Номер документу")
+    closed_by_documents = fields.Boolean(string=_("Closed by documents"), default=False)
+    original_document_number = fields.Char(string=_("Document number"))
 
     @api.onchange('closed_by_documents')
     def _onchange_closed_by_documents(self):
@@ -14,7 +14,7 @@ class StockPicking(models.Model):
             self.original_document_number = ''
 
     def action_done(self):
-        # Check if the 'Закрито документами' is checked and document number is not filled
+        # Check if the 'Closed by documents' is checked and document number is not filled
         if self.closed_by_documents and not self.original_document_number:
-            raise UserError("Будь ласка, заповніть оригінальний номер документу.")
+            raise UserError(_("Please fill in the original document number."))
         return super(StockPicking, self).action_done()
