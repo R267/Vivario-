49b20045e5f8215104f1abd6d4c2ce460fab9e7c
R267 <74021908+R267@users.noreply.github.com>
2024-10-31 09:32:15 +0200

    Update __manifest__.py

--- bs_vivario_addon/__manifest__.py
+++ bs_vivario_addon/__manifest__.py
@@ -1,19 +1,28 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'BonSens Vivario add-on',
+    'name': "BonSens Vivario add-on",
+
     'summary': """
-        BonSens add-on for Inventory""",
+        BonSens add-on for Vivario""",
+
     'description': """
-        BonSens add-on for Inventory""",
+        BonSens add-on for Vivario
+    """,
+
     'author': "BonSens",
     'website': "https://www.bonsens.com.ua",
-    'category': 'Inventory',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tools',
     'version': '17.0.1.0.1',
-    'depends': ['stock'],
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock'],
+
+    # always loaded
     'data': [
-         'views/delivery_order_view.xml',
+        'views/delivery_order_view.xml', 
     ],
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': True,
 }
